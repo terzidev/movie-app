d172c4940cdfff9d5e1c42d5a0f959c846645109
terzidev <tugrulterzi1@gmail.com>
2021-10-22 10:22:33 +0300

    Initial Commit

---
+++ MovieListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieListApp
+//
+//  Created by tailorr on 22.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
